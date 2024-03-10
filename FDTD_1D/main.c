278c31ee4cbc0120fa2524a550a06a2b26dd2780
Lherrera094 <herrera.woa94@gmail.com>
2024-03-10 08:22:35 -0600

    Update 1D

--- FDTD_1D/main.c
+++ FDTD_1D/main.c
@@ -6,8 +6,8 @@
 #define SIZE 200
 
 void writeFile(double mag[], double elec[], int filenumber){
-    char filename[20];
-    sprintf(filename, "data/time=%d.csv", filenumber);
+    char filename[40];
+    sprintf(filename, "ABC_data/time=%d.csv", filenumber);
 
     FILE *fptr = fopen(filename, "w");
 
@@ -20,30 +20,47 @@ void writeFile(double mag[], double elec[], int filenumber){
     for(int i = 0; i < SIZE-1; i++){
         fprintf(fptr, "%d,%.5f,%.5f\n",i, mag[i], elec[i]);
     }
-
     fclose(fptr);
 }
 
+double source_current(int time){
+    return exp(-(time - 30.) * (time-30.) / 100. );
+}//end source_current function
+
 int main(){
     
-    double ez[SIZE] = {0.}, hy[SIZE] = {0.}, imp0 = 377.0;
+    int activate_current = 1;
+    double ez[SIZE] = {0.}, hy[SIZE] = {0.}, jz[SIZE] = {0.} ,imp0 = 377.0;
     int qTime, maxTime = 250, mm;
 
     //Do time stepping
     for(qTime = 0; qTime < maxTime; qTime++){
-
+        
+        //Applying Absorbing Boundary Condition for hy[Size-1]
+        hy[SIZE-1] = hy[SIZE-2];
         // Update the magnetic field
         for(mm = 0; mm < SIZE - 1; mm++){
             hy[mm] = hy[mm] + ( (ez[mm+1] - ez[mm])/imp0 );
         }//end for magnetic field
 
+
+        //Applying an Absorbing Boundary Condition for ez[0]
+        ez[0] = ez[1];
         //Update electric field
         for(mm = 1; mm < SIZE; mm++){
             ez[mm] = ez[mm] + ( imp0*(hy[mm] - hy[mm-1]) );
         }//End for electric field 
 
-        //Hardwire a source node
-        ez[0] = exp(-(qTime - 30.) * (qTime-30.) / 100. );
+        //Habilitates current term in Ampere's Law
+        if( activate_current == 1){
+            double j = source_current(qTime);
+            ez[100] += j;
+        }else{
+            //Hardwire a source node
+            ez[50] += exp(-(qTime - 30.) * (qTime-30.) / 100. );
+        }
+
+        
 
         //printf("%g \n", ez[50]);
         writeFile(hy, ez, qTime);
