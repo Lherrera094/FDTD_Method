584a17b35d1545b72950b6f25d7514f7b1a20a24
Lherrera094 <herrera.woa94@gmail.com>
2024-04-30 11:46:44 -0600

    update 3d

---
+++ FDTD_3D/include/fdtd-proto.h
@@ -0,0 +1,23 @@
+
+#ifndef _FDTD_PROTO1_H
+#define _FDTD_PROTO1_H
+
+#include "fdtd.h"
+
+/*Function Prototypes*/
+void abcInit(Grid *g);
+void abc(Grid *g);
+
+void gridInit(Grid *g);
+
+void snapshotInit2d(Grid *g);
+void snapshot2d(Grid *g);
+
+/*void tfsfInit(Grid *g);
+void tfsfUpdate(Grid *g);
+*/
+
+void updateE2d(Grid *g);
+void updateH2d(Grid *g);
+
+#endif
---
+++ FDTD_3D/src/main.c
@@ -0,0 +1,68 @@
+/*TMz simulation with Ricker source at center of Grid*/
+
+#include "fdtd-alloc.h"
+#include "fdtd-macro.h"
+#include "fdtd-proto.h"
+#include "ezinc.h"
+
+void writeFile(Grid *g){
+    char filename[100];
+    sprintf(filename, "data/time=%d.csv", Time);
+
+    FILE *fptr = fopen(filename, "w");
+
+    if(fptr == NULL){
+        printf("Error opening the file. \n");
+    }
+
+    for(int i = 0; i < SizeX; i++){
+        fprintf(fptr, "%d,",i);
+    }
+    fprintf(fptr, "\n");
+
+    //pragma omp for
+    for(int nn = 0; nn < SizeY; nn++){
+        for(int mm = 0; mm < SizeX; mm++){
+            fprintf(fptr, "%.5f,", Ez(mm,nn));
+        }
+        fprintf(fptr, "\n"); 
+    }
+    
+    fclose(fptr);
+}
+
+int main() {
+
+    clock_t start, end;
+    double cpu_time_used;
+    start = clock();
+
+    Grid *g;
+
+    ALLOC_1D(g, 1, Grid);   // allocate memory for grid structure
+    gridInit(g);            // initialize 3D grid
+
+    abcInit(g);             // initialize ABC
+    ezIncInit(g);
+    snapshot3dInit(g);      // initialize snapshots
+
+    /* do time stepping */
+    for (Time = 0; Time < MaxTime; Time++) {
+    updateH(g);             // update magnetic fields
+    updateE(g);             // update electric fields
+
+    Ex((SizeX - 1) / 2, SizeY / 2, SizeZ / 2) += ezInc(Time, 0.0);
+    abc(g);                 // apply ABC
+    //snapshot3d(g);          // take a snapshot (if appropriate)
+    
+    writeFile(g);
+    
+    } // end of time-stepping
+
+    end = clock();
+    cpu_time_used = ( (double)(end-start) )/CLOCKS_PER_SEC;
+	printf("Program Running Time = %.2e s.\n", cpu_time_used);
+
+    return 0;
+
+}
